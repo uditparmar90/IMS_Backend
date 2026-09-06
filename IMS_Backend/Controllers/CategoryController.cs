8dcd863d8474fef2019c055ffdb9c1a50fb72ab1
Udit Parmar <udittparmar@gmail.com>
2026-09-06 19:34:54 +0530

    added API for product category

--- IMS_Backend/Controllers/CategoryController.cs
+++ IMS_Backend/Controllers/CategoryController.cs
@@ -15,15 +15,74 @@ namespace IMS_Backend.Controllers
         
 
         [HttpGet]
-        [Route("GetCategory")]
-        public IActionResult GetCategory()
+        [Route("GetAllCategories")]
+        public IActionResult GetCategories()
         {
             int UserId=Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
             var category = _context.ProdCategory.Where(c => c.UserId == UserId);
 
 
             return Ok(category);
-            //return View();
+        }
+        [HttpGet]
+        [Route("GetCategory")]
+        public IActionResult GetCategory(int Id)
+        {
+            int UserTd = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            ProductCategory record= _context.ProdCategory.FirstOrDefault(i=>i.UserId== UserTd && i.Id==Id);
+            return Ok(record);
+        }
+
+        [HttpPost]
+        [Route("Insert")]
+        public IActionResult Insert(ProductCategory category)
+        {
+            Console.WriteLine("You insert : "+category);
+             _context.ProdCategory.Add(category);
+            int RowsAffected=_context.SaveChanges();
+            bool IsInserted=RowsAffected > 0 ? true : false;
+            
+            return Ok(IsInserted); 
+        }
+        [HttpDelete]
+        [Route("Delete")]
+        public IActionResult Delete(int Id)
+        {
+            int UserId=Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            if (Id > 0)
+            {
+                ProductCategory obj = _context.ProdCategory.FirstOrDefault(item => item.UserId == UserId && item.Id == Id);
+
+                _context.ProdCategory.Remove(obj);
+                _context.SaveChanges();
+                return Ok(Id);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public IActionResult Update(ProductCategory category)
+        {
+            int UserId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            ProductCategory obj = _context.ProdCategory.FirstOrDefault(item => item.UserId == UserId && item.Id == category.Id);
+
+            if (obj!=null)
+            {
+                obj.Name = category.Name;
+
+                _context.ProdCategory.Update(obj);
+                _context.SaveChanges();
+                return Ok(obj.Id);
+            }
+            else
+            {
+                return NoContent();
+            }
         }
     }
 }
